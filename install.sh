a615739f436a7e67142f7a8e67fbbcc73c708a6d
root <root@instance-1.openstacklocal>
2022-05-04 11:49:25 +0000

    Firdt Commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
+[influxdb]
+name = InfluxDB Repository - RHEL \$releasever
+baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdb.key
+EOF
+cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+sudo yum update
+sudo dnf makecache
+sudo dnf -y install influxdb telegraf grafana
+sudo firewall-cmd --add-port=8086/tcp --permanent
+sudo firewall-cmd --reload
+sudo systemctl enable --now influxdb
+sudo systemctl enable --now grafana-server
