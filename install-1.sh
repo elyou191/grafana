5f016728d9638b8e01523cc7b86585b46a1cd1dc
elyou191 <elyou191@gmail.com>
2022-05-20 10:05:24 +0000

    shell file for debian distribution

---
+++ install-1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+wget -qO- https://repos.influxdata.com/influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
+export DISTRIB_ID=$(lsb_release -si); export DISTRIB_CODENAME=$(lsb_release -sc)
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list > /dev/null
+sudo apt-get update && sudo apt-get install influxdb2
+apt-get update
+apt-get install influxdb2 -y
+systemctl start influxdb
+systemctl enable influxdb
+
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+sudo apt update
+sudo apt install grafana -y
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
+
+
