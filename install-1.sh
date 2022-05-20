3a0419a77c13774a5a3b61b0c6a99b54434f43aa
elyou191 <elyou191@gmail.com>
2022-05-20 10:11:24 +0000

    shell file for debia distribution

--- install-1.sh
+++ install-1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+# Grafana dashboard http://ip:3000, username: admin, password: admin
+
+
+
 wget -qO- https://repos.influxdata.com/influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
 export DISTRIB_ID=$(lsb_release -si); export DISTRIB_CODENAME=$(lsb_release -sc)
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list > /dev/null
