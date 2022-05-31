629aba83f28f6784af1874c83985ae880aad13dd
root <root@localhost>
2022-05-31 08:08:31 -0600

    script shell for debian distribution

--- install-1.sh
+++ install-1.sh
@@ -8,7 +8,6 @@
 wget -qO- https://repos.influxdata.com/influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
 export DISTRIB_ID=$(lsb_release -si); export DISTRIB_CODENAME=$(lsb_release -sc)
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list > /dev/null
-sudo apt-get update && sudo apt-get install influxdb2
 apt-get update
 apt-get install influxdb2 -y
 systemctl start influxdb
